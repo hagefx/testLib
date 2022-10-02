7fcb1cbf3f071a29de796adc0b67cb5b83e77af3
hagefx <hage783640hage@yahoo.co.jp>
2022-10-02 14:32:38 +0900

    test

---
+++ src/util.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import numpy as np
+import pandas as pd
+
+
+def test():
+    x = np.random.randn(10)
+    return x
+
+
+def main():
+    print('hello testLib.util')
+
+
+if __name__ == '__main__':
+    main()
